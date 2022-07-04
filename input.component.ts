f47bac8b24e4772111b4829cb870b1c1d1fbeebc
Vlad <58708136+rooben4ik@users.noreply.github.com>
2022-07-04 12:57:48 +0300

    Update input.component.ts

--- input.component.ts
+++ input.component.ts
@@ -6,13 +6,13 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 	styleUrls: ['./input.component.scss']
 })
 export class InputComponent{
-	@Input() model;
+	@Input() wngModel;
 	@Input() label;
 	@Input() name = 'name';
 	@Input() type = 'text';
 	@Input() placeholder;
 	@Input() disabled;
-	@Output() modelChange = new EventEmitter()
+	@Output() wngModelChange = new EventEmitter()
 
 	constructor() {}
 }
